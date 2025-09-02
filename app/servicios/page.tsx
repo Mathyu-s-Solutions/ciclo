02bc36dcd152941593642a3e10de953b33487fe5
ChumanY <cuerpoespin_2000@hotmail.com>
2025-09-02 12:23:09 -0500

    fix: reorganize sections text and images

--- app/servicios/page.tsx
+++ app/servicios/page.tsx
@@ -21,39 +21,45 @@ export default function ServiciosPage() {
                         </div>
                     </div>
                     <div className="flex flex-col md:flex-row-reverse items-center gap-8">
-                        <div className="w-full md:w-1/2">
-                            <h2 className="text-base sm:text-xl lg:text-2xl text-left mb-4">Subtitulo 2</h2>
-                            <p className="mb-2">Medios digitales - Plataforma Recylink</p>
-                            <p className="mb-4">Body text for your whole article or post. We'll put in some lorem ipsum to show how a filled-out page might look:</p>
-                            <p className="mb-2">Beneficios Ambientales</p>
-                            <p className="mb-2">Body text for your whole article or post. We'll put in some lorem ipsum to show how a filled-out page might look:</p>
-                        </div>
-                        <div className="w-full md:w-1/2 flex justify-start">
-                            <Image src="/pages/servicios/5-Servicios2.webp" alt="Servicio 2" width={400} height={220} className="rounded-lg w-full max-w-md bg-gray-100 object-cover transition-transform duration-300 hover:-translate-y-2" />
+                        <div className="flex flex-col md:flex-row-reverse items-center gap-8">
+                            <div className="w-full md:w-1/2">
+                                <h2 className="text-base sm:text-xl lg:text-2xl text-left mb-4">Subtitulo 2</h2>
+                                <p className="mb-2">Medios digitales - Plataforma Recylink</p>
+                                <p className="mb-4">Body text for your whole article or post. We'll put in some lorem ipsum to show how a filled-out page might look:</p>
+                                <p className="mb-2">Beneficios Ambientales</p>
+                                <p className="mb-2">Body text for your whole article or post. We'll put in some lorem ipsum to show how a filled-out page might look:</p>
+                            </div>
+                            <div className="w-full md:w-1/2 flex justify-start">
+                                <Image src="/pages/servicios/5-Servicios2.webp" alt="Servicio 2" width={400} height={220} className="rounded-lg w-full max-w-md bg-gray-100 object-cover transition-transform duration-300 hover:-translate-y-2" />
+                            </div>
                         </div>
                     </div>
                     <div className="flex flex-col md:flex-row items-center gap-8">
-                        <div className="w-full md:w-1/2">
-                            <h2 className="text-base sm:text-xl lg:text-2xl text-left mb-4">Subtitulo 3</h2>
-                            <p className="mb-2">Descripción adicional</p>
-                            <p className="mb-4">Body text for your whole article or post. We'll put in some lorem ipsum to show how a filled-out page might look:</p>
-                            <p className="mb-2">Más beneficios</p>
-                            <p className="mb-2">Body text for your whole article or post. We'll put in some lorem ipsum to show how a filled-out page might look:</p>
-                        </div>
-                        <div className="w-full md:w-1/2 flex justify-end mb-6 md:mb-0">
-                            <Image src="/pages/servicios/5-Servicios.webp" alt="Servicio 3" width={400} height={220} className="rounded-lg w-full max-w-md bg-gray-100 object-cover transition-transform duration-300 hover:-translate-y-2" />
+                        <div className="flex flex-col md:flex-row items-center gap-8">
+                            <div className="w-full md:w-1/2">
+                                <h2 className="text-base sm:text-xl lg:text-2xl text-left mb-4">Subtitulo 3</h2>
+                                <p className="mb-2">Descripción adicional</p>
+                                <p className="mb-4">Body text for your whole article or post. We'll put in some lorem ipsum to show how a filled-out page might look:</p>
+                                <p className="mb-2">Más beneficios</p>
+                                <p className="mb-2">Body text for your whole article or post. We'll put in some lorem ipsum to show how a filled-out page might look:</p>
+                            </div>
+                            <div className="w-full md:w-1/2 flex justify-end mb-6 md:mb-0">
+                                <Image src="/pages/servicios/5-Servicios.webp" alt="Servicio 3" width={400} height={220} className="rounded-lg w-full max-w-md bg-gray-100 object-cover transition-transform duration-300 hover:-translate-y-2" />
+                            </div>
                         </div>
                     </div>
                     <div className="flex flex-col md:flex-row-reverse items-center gap-8">
-                        <div className="w-full md:w-1/2">
-                            <h2 className="text-base sm:text-xl lg:text-2xl text-left mb-4">Subtitulo 4</h2>
-                            <p className="mb-2">Título adicional</p>
-                            <p className="mb-4">Body text for your whole article or post. We'll put in some lorem ipsum to show how a filled-out page might look:</p>
-                            <p className="mb-2">Beneficio extra</p>
-                            <p className="mb-2">Body text for your whole article or post. We'll put in some lorem ipsum to show how a filled-out page might look:</p>
-                        </div>
-                        <div className="w-full md:w-1/2 flex justify-start mb-6 md:mb-0">
-                            <Image src="/pages/servicios/5-Servicios2.webp" alt="Servicio 4" width={400} height={220} className="rounded-lg w-full max-w-md bg-gray-100 object-cover transition-transform duration-300 hover:-translate-y-2" />
+                        <div className="flex flex-col md:flex-row-reverse items-center gap-8">
+                            <div className="w-full md:w-1/2">
+                                <h2 className="text-base sm:text-xl lg:text-2xl text-left mb-4">Subtitulo 4</h2>
+                                <p className="mb-2">Título adicional</p>
+                                <p className="mb-4">Body text for your whole article or post. We'll put in some lorem ipsum to show how a filled-out page might look:</p>
+                                <p className="mb-2">Beneficio extra</p>
+                                <p className="mb-2">Body text for your whole article or post. We'll put in some lorem ipsum to show how a filled-out page might look:</p>
+                            </div>
+                            <div className="w-full md:w-1/2 flex justify-start mb-6 md:mb-0">
+                                <Image src="/pages/servicios/5-Servicios2.webp" alt="Servicio 4" width={400} height={220} className="rounded-lg w-full max-w-md bg-gray-100 object-cover transition-transform duration-300 hover:-translate-y-2" />
+                            </div>
                         </div>
                     </div>
                 </div>
